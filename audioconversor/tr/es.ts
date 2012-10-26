89b7ebcb8c472bbbe53949884b77f304f95ac75a
adonaysanz <adonaysanz@5b1807c8-517e-0410-8a1f-a547194cc93c>
2012-10-26 17:42:08 +0000

    The Last SVN Update of DEBIAN. Now all will be deleted.
    Beep Beep - End of Message - Beep Beep - End of Debian Repo - Beep Beep

--- audioconversor/tr/es.ts
+++ audioconversor/tr/es.ts
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="es_ES">
+<!DOCTYPE TS><TS version="2.0" language="es_ES" sourcelanguage="">
 <context>
     <name>AudioConversor</name>
     <message>

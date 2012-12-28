7c4124fdc00f8258ea2e6199164e042ffd8a55b5
adonaysanz <adonaysanz@5b1807c8-517e-0410-8a1f-a547194cc93c>
2012-12-28 12:19:52 +0000

    Changed icon for an older version Remove of instalador 5 and meta now not fails Store a copy of dmesg on log to know if there are read errors Fixed links of preload Added teamviewer service

--- apps/instalador-old/instalador/prepare.sh
+++ apps/instalador-old/instalador/prepare.sh
@@ -2,9 +2,9 @@
 
 #Execute only from  makefile of kademar-base
 
-echo "########################"
-echo "###   INSTALADOR 5   ###"
-echo "########################"
+echo "##########################"
+echo "###   INSTALADOR 4.5   ###"
+echo "##########################"
 
 #Si s'ha executat des del makefile, entra a la carpeta correcta
 [ "$0" = "../../instalador/prepare.sh" ] && cd ../../instalador
